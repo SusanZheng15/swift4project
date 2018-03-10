5de5baed1a34becec82b4f6fd2d02545267eb8b1
Susan Zheng <Susan@MacBook-Pro.local>
2018-03-10 14:32:20 -0500

    Initial Commit

---
+++ swift4project/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  swift4project
+//
+//  Created by Susan Zheng on 3/10/18.
+//  Copyright © 2018 Susan Zheng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
